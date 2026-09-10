25032cbb435f26cee93eae53baadf7d963e3ddd2
Ancy Lazar <ancylazar@gmail.com>
2026-09-10 21:18:58 +0530

    Fix app Python syntax

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 def find_max(numbers):
-     return max(numbers) 
+    return max(numbers)
 def count_evens(numbers):
-     return len([n forn innumbers ifn% 2 ==0])
+    return len([n for n in numbers if n % 2 == 0])
